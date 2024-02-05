981c90fba99fe7780aff8ba0be8a4ae5f7678b7b
anubhav-aryan <aryananubhav08@gmail.com>
2024-02-05 22:26:46 +0530

    feat: testimonials

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import Header from "@/components/Header";
+import Testimonial from "@/components/Testimonial";
 import React from "react";
 
 const index = () => {
   return (
     <>
       <Header />
+      {/* <Testimonial /> */}
     </>
   );
 };
