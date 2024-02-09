36e5c1e92b4a7859608cf87805e0c4e5ebe47e34
anubhav-aryan <aryananubhav08@gmail.com>
2024-02-09 15:47:59 +0530

    fix: padding

--- components/AnalyticLabel.tsx
+++ components/AnalyticLabel.tsx
@@ -11,18 +11,18 @@ const AnalyticLabel = () => {
       </div>
       <div className="line w-[2px] bg-[rgba(255,255,255,0.2)]"></div>
       <div className="label flex flex-col items-center gap-2">
-        <h1 className="text-5xl text-white font-bold">2,300+</h1>
+        <h1 className="text-5xl text-white font-bold">500+</h1>
         <h3 className="text-[#11b7db] text-lg font-semibold">
           Happy Patients a week
         </h3>
       </div>{" "}
-      <div className="line w-[2px] bg-[rgba(255,255,255,0.2)]"></div>
-      <div className="label flex flex-col items-center gap-2">
+      {/* <div className="line w-[2px] bg-[rgba(255,255,255,0.2)]"></div> */}
+      {/* <div className="label flex flex-col items-center gap-2">
         <h1 className="text-5xl text-white font-bold">43</h1>
         <h3 className="text-[#11b7db] text-lg font-semibold">
           Professional Doctors
         </h3>
-      </div>
+      </div> */}
     </div>
   );
 };
